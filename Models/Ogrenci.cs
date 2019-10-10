a2ee096dda1cd927baa293383c4771ee1739ff8b
merve <36377574+merveealpay@users.noreply.github.com>
2019-10-10 22:56:33 +0300

    add models

---
+++ Models/Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorNedir.Models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public int OgrenciNo { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Adres { get; set; }
+        public int SinifId { get; set; }
+    }
+}
